ff5186bb15365ea404aa9771b521594604210056
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2023-12-12 17:05:12 -0800

    Fix convert.py support for Mistral-7B model
    Mistral model has 32 heads and 8 kv-heads, so we need to generalize our
    permute reverse code; for now we keep number of heads hardcoded, which
    could be fixed separately.

--- tools/convert.py
+++ tools/convert.py
@@ -2,8 +2,10 @@
 # python convert.py merged.safetensors --models file1.safetensors file2.safetensors ...
 
 import argparse
+import os.path
 import safetensors
 import safetensors.torch
+import torch
 
 args = argparse.ArgumentParser()
 args.add_argument("output", type=str)
@@ -12,10 +14,19 @@ args = args.parse_args()
 
 tensors = {}
 for fn in args.models:
-    with safetensors.safe_open(fn, framework="pt") as f:
-        for k in f.keys():
+    ext = os.path.splitext(fn)[1]
+    if ext == ".safetensors":
+        with safetensors.safe_open(fn, framework="pt") as f:
+            for k in f.keys():
+                assert(k not in tensors)
+                tensors[k] = f.get_tensor(k)
+    elif ext == ".bin":
+        weights = torch.load(fn, weights_only=True)
+        for k in weights.keys():
             assert(k not in tensors)
-            tensors[k] = f.get_tensor(k)
+            tensors[k] = weights[k]
+    else:
+        raise Exception("Unknown file extension: {}; expected .safetensors or .bin".format(ext))
 
 # huggingface permutes WQ and WK, this function reverses it
 # see https://github.com/huggingface/transformers/blob/b132c1703eb1c8bd9dfa4ad6a9be2bfd6ef819e9/src/transformers/models/llama/convert_llama_weights_to_hf.py#L122
@@ -25,8 +36,9 @@ def permute_reverse(w, heads, dim1, dim2):
 for k, v in tensors.items():
     if "self_attn.q_proj" in k or "self_attn.k_proj" in k:
         n_heads = 32
-        dim = v.shape[0]
-        v = permute_reverse(v, n_heads, dim, dim)
+        dim1 = v.shape[0]
+        dim2 = v.shape[1]
+        v = permute_reverse(v, n_heads // (dim2 // dim1), dim1, dim2)
 
     # convert to f32 for now
     tensors[k] = v.float()
