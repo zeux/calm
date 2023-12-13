aeb907b9c19ef2af672a1f17724bb0d5959d7ed3
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2023-12-13 12:17:25 -0800

    Parse model config files and save important parameters via metadata
    This means that we can no longer work directly with HF .safetensors
    files but they simply don't contain some important information, and we
    might have to do deeper tensor modifications in the future anyway.

--- tools/convert.py
+++ tools/convert.py
@@ -1,7 +1,8 @@
 # Produce a safetensors model file out of multiple inputs
-# python convert.py merged.safetensors --models file1.safetensors file2.safetensors ...
+# python convert.py model.safetensors --config config.json --models file1.bin file2.bin ...
 
 import argparse
+import json
 import os.path
 import safetensors
 import safetensors.torch
@@ -9,10 +10,33 @@ import torch
 
 args = argparse.ArgumentParser()
 args.add_argument("output", type=str)
+args.add_argument("--config", type=str, required=True)
 args.add_argument("--models", type=str, nargs="+", required=True)
 args.add_argument("--dtype", type=str, default="float16", choices=["bfloat16", "float16", "float32"])
 args = args.parse_args()
 
+with open(args.config, "r") as f:
+    config = json.load(f)
+
+metadata = {}
+
+# hardcoded in C
+assert config["hidden_act"] == "silu"
+assert config["bos_token_id"] == 1
+assert config["eos_token_id"] == 2
+assert config["rms_norm_eps"] == 1e-5
+
+# customizable
+metadata["dim"] = config["hidden_size"]
+metadata["hidden_dim"] = config["intermediate_size"]
+metadata["n_layers"] = config["num_hidden_layers"]
+metadata["n_heads"] = config["num_attention_heads"]
+metadata["n_kv_heads"] = config["num_key_value_heads"]
+metadata["vocab_size"] = config["vocab_size"]
+if "rope_theta" in config:
+    metadata["rope_theta"] = config["rope_theta"]
+
+# load model files
 tensors = {}
 for fn in args.models:
     ext = os.path.splitext(fn)[1]
@@ -36,6 +60,7 @@ def permute_reverse(w, heads, dim1, dim2):
 
 dtype = {"bfloat16": torch.bfloat16, "float16": torch.float16, "float32": torch.float32}[args.dtype]
 
+# convert tensors to dtype and permute if necessary
 for k, v in tensors.items():
     if "self_attn.q_proj" in k or "self_attn.k_proj" in k:
         n_heads = 32
@@ -45,4 +70,5 @@ for k, v in tensors.items():
 
     tensors[k] = v.to(dtype)
 
-safetensors.torch.save_file(tensors, args.output)
+# metadata values must be strings in safetensors
+safetensors.torch.save_file(tensors, args.output, {k: str(v) for k, v in metadata.items()})
