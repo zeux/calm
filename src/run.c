3ffbb6e6b614c600b87650c55fdae6cd5855762e
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2023-12-22 09:13:57 -0800

    Remove chat mode for now
    This is untested and requires a lot of tweaks wrt prompts and code
    cleanup, so it would be better to completely delete this for now and
    maybe reintroduce in the future.

--- src/run.c
+++ src/run.c
@@ -560,110 +560,6 @@ void generate(struct Transformer* transformer, Tokenizer* tokenizer, Sampler* sa
 	free(prompt_tokens);
 }
 
-void read_stdin(const char* guide, char* buffer, size_t bufsize) {
-	// read a line from stdin, up to but not including \n
-	printf("%s", guide);
-	if (fgets(buffer, bufsize, stdin) != NULL) {
-		size_t len = strlen(buffer);
-		if (len > 0 && buffer[len - 1] == '\n') {
-			buffer[len - 1] = '\0'; // strip newline
-		}
-	}
-}
-
-// ----------------------------------------------------------------------------
-// chat loop
-// I manually inspected the tokens for a few chat conversations compared to
-// python reference and that seemed ok, but this was not thoroughly tested and
-// is not safely implemented, it's more a proof of concept atm.
-
-void chat(struct Transformer* transformer, Tokenizer* tokenizer, Sampler* sampler,
-          char* cli_user_prompt, char* cli_system_prompt, int steps) {
-
-	// buffers for reading the system prompt and user prompt from stdin
-	// you'll notice they are soomewhat haphazardly and unsafely set atm
-	char system_prompt[512];
-	char user_prompt[512];
-	char rendered_prompt[1152];
-	int num_prompt_tokens = 0;
-	int* prompt_tokens = (int*)malloc(1152 * sizeof(int));
-	int user_idx;
-
-	// start the main loop
-	int8_t user_turn = 1; // user starts
-	int next = -1;        // will store the next token in the sequence
-	int token;            // stores the current token to feed into the transformer
-	int pos = 0;          // position in the sequence
-	while (pos < steps) {
-
-		// when it is the user's turn to contribute tokens to the dialog...
-		if (user_turn) {
-			// get the (optional) system prompt at position 0
-			if (pos == 0) {
-				// at position 0, the user can also contribute a system prompt
-				if (cli_system_prompt == NULL) {
-					// system prompt was not passed in, attempt to get it from stdin
-					read_stdin("Enter system prompt (optional): ", system_prompt, sizeof(system_prompt));
-				} else {
-					// system prompt was passed in, use it
-					strcpy(system_prompt, cli_system_prompt);
-				}
-			}
-			// get the user prompt
-			if (pos == 0 && cli_user_prompt != NULL) {
-				// user prompt for position 0 was passed in, use it
-				strcpy(user_prompt, cli_user_prompt);
-			} else {
-				// otherwise get user prompt from stdin
-				read_stdin("User: ", user_prompt, sizeof(user_prompt));
-			}
-			// render user/system prompts into the Llama 2 Chat schema
-			if (pos == 0 && system_prompt[0] != '\0') {
-				char system_template[] = "[INST] <<SYS>>\n%s\n<</SYS>>\n\n%s [/INST]";
-				sprintf(rendered_prompt, system_template, system_prompt, user_prompt);
-			} else {
-				char user_template[] = "[INST] %s [/INST]";
-				sprintf(rendered_prompt, user_template, user_prompt);
-			}
-			// encode the rendered prompt into tokens
-			encode(tokenizer, rendered_prompt, 1, 0, prompt_tokens, &num_prompt_tokens);
-			user_idx = 0; // reset the user index
-			user_turn = 0;
-			printf("Assistant: ");
-		}
-
-		// determine the token to pass into the transformer next
-		if (user_idx < num_prompt_tokens) {
-			// if we are still processing the input prompt, force the next prompt token
-			token = prompt_tokens[user_idx++];
-		} else {
-			// otherwise use the next token sampled from previous turn
-			token = next;
-		}
-		// EOS (=2) token ends the Assistant turn
-		if (token == 2) {
-			user_turn = 1;
-		}
-
-		// forward the transformer to get logits for the next token
-		float* logits = transformer->forward(transformer, token, pos);
-		next = sample(sampler, logits);
-		pos++;
-
-		if (user_idx >= num_prompt_tokens && next != 2) {
-			// the Assistant is responding, so print its output
-			char* piece = decode(tokenizer, token, next);
-			safe_printf(piece); // same as printf("%s", piece), but skips "unsafe" bytes
-			fflush(stdout);
-		}
-		if (next == 2) {
-			printf("\n");
-		}
-	}
-	printf("\n");
-	free(prompt_tokens);
-}
-
 // ----------------------------------------------------------------------------
 // CLI, include only if not testing
 #ifndef TESTING
@@ -678,8 +574,6 @@ void error_usage() {
 	fprintf(stderr, "  -n <int>    number of steps to run for, default 256. 0 = max_seq_len\n");
 	fprintf(stderr, "  -r <int>    number of sequences to decode, default 1\n");
 	fprintf(stderr, "  -i <string> input prompt\n");
-	fprintf(stderr, "  -m <string> mode: generate|chat, default: generate\n");
-	fprintf(stderr, "  -y <string> (optional) system prompt in chat mode\n");
 	fprintf(stderr, "  -a <string> use accelerator (cpu, cuda)\n");
 	exit(EXIT_FAILURE);
 }
@@ -694,8 +588,6 @@ int main(int argc, char* argv[]) {
 	int sequences = 1;               // number of sequences to decode
 	char* prompt = NULL;             // prompt string
 	unsigned long long rng_seed = 0; // seed rng with time by default
-	char* mode = "generate";         // generate|chat
-	char* system_prompt = NULL;      // the (optional) system prompt to use in chat mode
 	char* accelerator = "cpu";       // cpu|cuda
 
 	// poor man's C argparse so we can override the defaults above from the command line
@@ -728,10 +620,6 @@ int main(int argc, char* argv[]) {
 			sequences = atoi(argv[i + 1]);
 		} else if (argv[i][1] == 'i') {
 			prompt = argv[i + 1];
-		} else if (argv[i][1] == 'm') {
-			mode = argv[i + 1];
-		} else if (argv[i][1] == 'y') {
-			system_prompt = argv[i + 1];
 		} else if (argv[i][1] == 'a') {
 			accelerator = argv[i + 1];
 		} else {
@@ -782,15 +670,8 @@ int main(int argc, char* argv[]) {
 	build_sampler(&sampler, transformer.config.vocab_size, temperature, topp, rng_seed);
 
 	// run!
-	if (strcmp(mode, "generate") == 0) {
-		for (int s = 0; s < sequences; ++s) {
-			generate(&transformer, &tokenizer, &sampler, prompt, steps);
-		}
-	} else if (strcmp(mode, "chat") == 0) {
-		chat(&transformer, &tokenizer, &sampler, prompt, system_prompt, steps);
-	} else {
-		fprintf(stderr, "unknown mode: %s\n", mode);
-		error_usage();
+	for (int s = 0; s < sequences; ++s) {
+		generate(&transformer, &tokenizer, &sampler, prompt, steps);
 	}
 
 	// memory and file handles cleanup
