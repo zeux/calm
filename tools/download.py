9e23669a398a941a9d87b24efa96ab40ec4f5b18
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2023-12-22 13:09:23 -0800

    tools: Remove token=true from download.py
    token=True doesn't work without huggingface_hub login, which is a
    problem because it prevents using download.py for downloading open
    models like Mistral.

--- tools/download.py
+++ tools/download.py
@@ -10,4 +10,4 @@ argp.add_argument("repo", type=str)
 args = argp.parse_args()
 
 # download model folder from HuggingFace, excluding .bin files (assume the model contains safetensors)
-huggingface_hub.snapshot_download(repo_id=args.repo, local_dir=args.output, local_dir_use_symlinks=False, resume_download=True, token=True, ignore_patterns="*.bin")
+huggingface_hub.snapshot_download(repo_id=args.repo, local_dir=args.output, local_dir_use_symlinks=False, resume_download=True, ignore_patterns="*.bin")
