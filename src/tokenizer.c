ebc13c3bcd81a1699d16d17b887f4044ecac4572
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2023-12-30 17:20:09 -0800

    Clean up the rest of tokenizer_encode
    Instead of parsing the input byte-by-byte, parse it codepoint by
    codepoint; this simplifies the function flow significantly.

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -187,81 +187,52 @@ static int merge_tokens(struct Tokenizer* tokenizer, int* tokens, int n_tokens) 
 }
 
 int tokenizer_encode(struct Tokenizer* tokenizer, char* text, unsigned flags, int* tokens) {
-	// encode the string text (input) into an upper-bound preallocated tokens[] array
-	assert(text);
 	int n_tokens = 0;
 
-	// create a temporary buffer that will store merge candidates of always two consecutive tokens
-	// *2 for concat, +1 for null terminator
-	char str_buffer[MAX_TOKEN_LENGTH * 2 + 1];
-	size_t str_len = 0;
-
 	// add optional BOS token, if desired
-	if (flags & TF_ENCODE_BOS)
+	if (flags & TF_ENCODE_BOS) {
 		tokens[n_tokens++] = tokenizer->bos_id;
+	}
 
-	// add_dummy_prefix is true by default
-	// so prepend a dummy prefix token to the input string, but only if text != ""
-	// TODO: pretty sure this isn't correct in the general case but I don't have the
-	// energy to read more of the sentencepiece code to figure out what it's doing
+	// assuming add_dummy_prefix is true, prepend a dummy prefix token to the input string, but only if text != ""
 	if (text[0] != '\0') {
 		int dummy_prefix = str_lookup(" ", tokenizer->sorted_vocab, tokenizer->vocab_size);
 		tokens[n_tokens++] = dummy_prefix;
 	}
 
-	// Okay UTF-8 time. This will get messy. Here is the reference from Wikipedia:
-	// Code point ↔ UTF-8 conversion
-	// First code point	Last code point	Byte 1	Byte 2	Byte 3	Byte 4
-	// U+0000	U+007F	    0xxxxxxx
-	// U+0080	U+07FF	    110xxxxx	10xxxxxx
-	// U+0800	U+FFFF	    1110xxxx	10xxxxxx	10xxxxxx
-	// U+10000	U+10FFFF    11110xxx	10xxxxxx	10xxxxxx	10xxxxxx
-
 	// process the raw (UTF-8) byte sequence of the input string
-	for (char* c = text; *c != '\0'; c++) {
+	for (char* c = text; *c != '\0';) {
+		char codepoint[5] = {};
 
-		// reset buffer if the current byte is ASCII or a leading byte
-		// 0xC0 is 11000000, so (*c & 0xC0) keeps the first 2 bits and zeros the rest
-		// 0x80 is 10000000
-		// in UTF-8, all continuation bytes start with "10" in first two bits
-		// so in English this is: "if this byte is not a continuation byte"
-		if ((*c & 0xC0) != 0x80) {
-			// this byte must be either a leading byte (11...) or an ASCII char (0x...)
-			// => reset our location, as we're starting a new UTF-8 codepoint
-			str_len = 0;
+		codepoint[0] = *c++;
+
+		// this byte is a leading byte (11...), so it's a multi-byte UTF8 codepoint
+		if ((codepoint[0] & 0xC0) == 0xC0) {
+			for (int i = 1; i < 4 && (*c & 0xC0) == 0x80; ++i) {
+				codepoint[i] = *c++;
+			}
 		}
 
-		// append the current byte to the buffer
-		str_buffer[str_len++] = *c; // ++ is post-increment, incremented after this line
-		str_buffer[str_len] = '\0';
-
-		// while the next character is a continuation byte, continue appending
-		// but if there are too many of them, just stop to avoid overruning str_buffer size.
-		if ((*(c + 1) & 0xC0) == 0x80 && str_len < 4) {
-			continue;
-		}
-
-		// ok c+1 is not a continuation byte, so we've read in a full codepoint
-		int id = str_lookup(str_buffer, tokenizer->sorted_vocab, tokenizer->vocab_size);
+		int id = str_lookup(codepoint, tokenizer->sorted_vocab, tokenizer->vocab_size);
 
 		if (id != -1) {
 			// we found this codepoint in vocab, add it as a token
 			tokens[n_tokens++] = id;
 		} else if (tokenizer->byte_fallbacks >= 0) {
 			// byte_fallback encoding: just encode each byte as a token
-			for (int i = 0; i < str_len; i++) {
-				tokens[n_tokens++] = (unsigned char)str_buffer[i] + tokenizer->byte_fallbacks;
+			for (char* fb = codepoint; *fb != '\0'; ++fb) {
+				tokens[n_tokens++] = (unsigned char)*fb + tokenizer->byte_fallbacks;
 			}
 		}
-		str_len = 0; // protect against a sequence of stray UTF8 continuation bytes
 	}
 
 	// optimized heap-based merge
 	n_tokens = merge_tokens(tokenizer, tokens, n_tokens);
 
 	// add optional EOS token, if desired
-	if (flags & TF_ENCODE_EOS)
+	if (flags & TF_ENCODE_EOS) {
 		tokens[n_tokens++] = tokenizer->eos_id;
+	}
 
 	assert(n_tokens <= tokenizer_bound(strlen(text)));
 	return n_tokens;
