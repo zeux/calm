f37e096a7137851178aa38493e3be7b316115caa
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2024-01-08 11:19:20 -0800

    tools: Implement conversion support for Qwen models
    Qwen models are more or less Llamas, but...
    
    - The tokenizer is stored in tiktoken format plus some Python code to
      insert special tokens. We parse tiktoken now but ignore the code.
    - The layer names are all different
    - The QKV matrices are fused
    - The QKV matrices have a bias
    
    With the exception of the last bit, all of the rest are just a matter of
    some adjustments to Python code.
    
    Note: to convert Qwen, you need to pass tokenizer path manually as it's
    not generically discoverable from the model folder.

--- tools/convert.py
+++ tools/convert.py
@@ -2,6 +2,7 @@
 # python convert.py model.safetensors --config config.json --models file1.bin file2.bin ...
 
 import argparse
+import base64
 import json
 import os.path
 import safetensors
@@ -47,7 +48,7 @@ metadata = {}
 tensors = {}
 
 arch = config["architectures"][0]
-arch_remap = {"LlamaForCausalM": "llama", "MistralForCausalLM": "mistral", "PhiForCausalLM": "phi"}
+arch_remap = {"LlamaForCausalM": "llama", "MistralForCausalLM": "mistral", "PhiForCausalLM": "phi", "QWenLMHeadModel": "qwen"}
 assert arch in arch_remap, "Unsupported architecture: {}; must be one of: {}".format(arch, list(arch_remap.keys()))
 arch = arch_remap[arch]
 
@@ -69,6 +70,19 @@ if arch in ["llama", "mistral"]:
     metadata["max_seq_len"] = config["max_position_embeddings"]
     metadata["bos_token_id"] = config["bos_token_id"]
     metadata["eos_token_id"] = config["eos_token_id"]
+    metadata["rope_theta"] = config.get("rope_theta", 10000.0)
+    metadata["rotary_dim"] = config["hidden_size"] // config["num_attention_heads"]
+elif arch == "qwen":
+    # customizable
+    metadata["dim"] = config["hidden_size"]
+    metadata["hidden_dim"] = config["intermediate_size"] // 2
+    metadata["n_layers"] = config["num_hidden_layers"]
+    metadata["n_heads"] = config["num_attention_heads"]
+    metadata["n_kv_heads"] = config["num_attention_heads"]
+    metadata["vocab_size"] = config["vocab_size"]
+    metadata["max_seq_len"] = config["seq_length"]
+    metadata["bos_token_id"] = -1
+    metadata["eos_token_id"] = 151643 # <|endoftext|> hardcoded in tokenization_qwen.py
     metadata["rope_theta"] = config.get("rope_theta", 10000.0)
     metadata["rotary_dim"] = config["hidden_size"] // config["num_attention_heads"]
 elif arch == "phi":
@@ -120,8 +134,18 @@ elif ext == ".model":
     for i in range(sp_model.vocab_size()):
         tokens[i] = sp_model.id_to_piece(i)
         scores[i] = sp_model.get_score(i)
+elif ext == ".tiktoken":
+    with open(args.tokenizer, "r") as f:
+        vocab = f.readlines()
+    assert len(vocab) <= config["vocab_size"]
+
+    for i, l in enumerate(vocab):
+        t, r = l.rstrip().split(" ")
+        t = base64.b64decode(t)
+        tokens[i] = t.decode("utf-8", errors="replace").replace("\0", "\7")
+        scores[i] = -int(r)
 else:
-    raise Exception("Unknown tokenizer file extension: {}; expected .json or .model".format(ext))
+    raise Exception("Unknown tokenizer file extension: {}; expected .json or .model/.tiktoken".format(ext))
 
 # postprocess tokens
 for i, t in enumerate(tokens):
@@ -200,6 +224,35 @@ if arch in ["llama", "mistral"]:
         tensors[f"model.layers.{l}.mlp.w3.weight"] = conv(weights[f"model.layers.{l}.mlp.up_proj.weight"])
 
     tensors["model.norm.weight"] = weights["model.norm.weight"].float()
+    tensors["model.output.weight"] = conv(weights["lm_head.weight"])
+elif arch == "qwen":
+    tensors["model.embed.weight"] = conv(weights["transformer.wte.weight"])
+
+    for l in range(config["num_hidden_layers"]):
+        tensors[f"model.layers.{l}.attn.norm.weight"] = weights[f"transformer.h.{l}.ln_1.weight"].float()
+
+        dim = config["hidden_size"]
+        head_dim = dim // config["num_attention_heads"]
+
+        wkv_w = weights[f"transformer.h.{l}.attn.c_attn.weight"]
+        wkv_b = weights[f"transformer.h.{l}.attn.c_attn.bias"]
+        assert wkv_w.shape[0] == 3 * dim and wkv_b.shape[0] == 3 * dim
+
+        tensors[f"model.layers.{l}.attn.wq.weight"] = conv(permute_reverse(wkv_w[:dim], config["num_attention_heads"], head_dim))
+        tensors[f"model.layers.{l}.attn.wq.bias"] = permute_reverse(wkv_b[:dim], config["num_attention_heads"], head_dim).float()
+        tensors[f"model.layers.{l}.attn.wk.weight"] = conv(permute_reverse(wkv_w[dim:dim*2], config["num_attention_heads"], head_dim))
+        tensors[f"model.layers.{l}.attn.wk.bias"] = permute_reverse(wkv_b[dim:dim*2], config["num_attention_heads"], head_dim).float()
+        tensors[f"model.layers.{l}.attn.wv.weight"] = conv(wkv_w[dim*2:])
+        tensors[f"model.layers.{l}.attn.wv.bias"] = wkv_b[dim*2:].float()
+        tensors[f"model.layers.{l}.attn.wo.weight"] = conv(weights[f"transformer.h.{l}.attn.c_proj.weight"])
+
+        tensors[f"model.layers.{l}.mlp.norm.weight"] = weights[f"transformer.h.{l}.ln_2.weight"].float()
+
+        tensors[f"model.layers.{l}.mlp.w1.weight"] = conv(weights[f"transformer.h.{l}.mlp.w2.weight"])
+        tensors[f"model.layers.{l}.mlp.w2.weight"] = conv(weights[f"transformer.h.{l}.mlp.c_proj.weight"])
+        tensors[f"model.layers.{l}.mlp.w3.weight"] = conv(weights[f"transformer.h.{l}.mlp.w1.weight"])
+
+    tensors["model.norm.weight"] = weights["transformer.ln_f.weight"].float()
     tensors["model.output.weight"] = conv(weights["lm_head.weight"])
 elif arch == "phi":
     tensors["model.embed.weight"] = conv(weights["transformer.embd.wte.weight"])
