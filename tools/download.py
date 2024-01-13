8b2ae14e1fa8ad3b842c0e3df91fb1ea59184fc8
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2024-01-12 18:19:40 -0800

    tools: Add --all argument to download.py
    This is helpful when downloading models without .safetensors tensors.

--- tools/download.py
+++ tools/download.py
@@ -7,7 +7,9 @@ import huggingface_hub
 argp = argparse.ArgumentParser()
 argp.add_argument("output", type=str)
 argp.add_argument("repo", type=str)
+argp.add_argument("--all", action="store_true")
 args = argp.parse_args()
 
 # download model folder from HuggingFace, excluding .bin files (assume the model contains safetensors)
-huggingface_hub.snapshot_download(repo_id=args.repo, local_dir=args.output, local_dir_use_symlinks=False, resume_download=True, ignore_patterns=["*.bin", "*.pth", "*.pt"])
+ignore_patterns = ["*.bin", "*.pth", "*.pt"] if not args.all else []
+huggingface_hub.snapshot_download(repo_id=args.repo, local_dir=args.output, local_dir_use_symlinks=False, resume_download=True, ignore_patterns=ignore_patterns)
