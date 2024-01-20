a318845af9fc2717fcafe2d5644898f8677731c8
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2024-01-20 09:12:27 -0800

    tools: Exclude gguf from download by default
    Some repositories contain gguf as well as safetensors.

--- tools/download.py
+++ tools/download.py
@@ -11,5 +11,5 @@ argp.add_argument("--all", action="store_true")
 args = argp.parse_args()
 
 # download model folder from HuggingFace, excluding .bin files (assume the model contains safetensors)
-ignore_patterns = ["*.bin", "*.pth", "*.pt"] if not args.all else []
+ignore_patterns = ["*.bin", "*.pth", "*.pt", "*.gguf"] if not args.all else []
 huggingface_hub.snapshot_download(repo_id=args.repo, local_dir=args.output, local_dir_use_symlinks=False, resume_download=True, ignore_patterns=ignore_patterns)
