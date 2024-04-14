cc90901841d2553ba9fc23245d9c1143958bf267
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2024-04-13 19:21:38 -0700

    Metal: Add support for dynamic threadgroup size
    We don't use this yet but it may be necessary to improve performance for gf4 kernels; specifying
    this for every launch doesn't seem to carry visible overhead.

--- src/infer.m
+++ src/infer.m
@@ -12,7 +12,7 @@ static const char* kernel_names[256];
 
 static MTLCaptureManager* capture;
 
-static void dispatch(id<MTLComputeCommandEncoder> encoder, const char* name, const char* variant, unsigned int thread_groups, unsigned int thread_group_size, void* params, size_t params_size, void** buffers, size_t buffer_count) {
+static void dispatch(id<MTLComputeCommandEncoder> encoder, const char* name, const char* variant, unsigned int threadgroups, unsigned int threadgroup_size, unsigned int threadgroup_smem, void* params, size_t params_size, void** buffers, size_t buffer_count) {
 	char expected[256];
 	strcpy(expected, name);
 	if (variant) {
@@ -28,15 +28,15 @@ static void dispatch(id<MTLComputeCommandEncoder> encoder, const char* name, con
 		}
 	}
 	assert(state);
-	assert(state.maxTotalThreadsPerThreadgroup >= thread_group_size);
+	assert(state.maxTotalThreadsPerThreadgroup >= threadgroup_size);
 
 	static const NSUInteger offsets[16] = {};
 
 	[encoder setComputePipelineState:state];
 	[encoder setBytes:params length:params_size atIndex:0];
 	[encoder setBuffers:(const id<MTLBuffer>*)buffers offsets:offsets withRange:NSMakeRange(1, buffer_count)];
-
-	[encoder dispatchThreadgroups:MTLSizeMake(thread_groups, 1, 1) threadsPerThreadgroup:MTLSizeMake(thread_group_size, 1, 1)];
+	[encoder setThreadgroupMemoryLength:threadgroup_smem atIndex:0];
+	[encoder dispatchThreadgroups:MTLSizeMake(threadgroups, 1, 1) threadsPerThreadgroup:MTLSizeMake(threadgroup_size, 1, 1)];
 }
 
 void init_metal(void) {
@@ -206,11 +206,11 @@ float* forward_metal(struct Transformer* transformer, int token, int pos, unsign
 
 	// copy the token embedding into x
 	assert(token < p->vocab_size);
-	dispatch(encoder, "embed", dvar, dim / 32, 32, (int[]){ token * dim }, sizeof(int), (void*[]){ x, w->token_embedding_table }, 2);
+	dispatch(encoder, "embed", dvar, dim / 32, 32, 0, (int[]){ token * dim }, sizeof(int), (void*[]){ x, w->token_embedding_table }, 2);
 
 	// rotate sink tokens forward to keep pace with non-sink tokens
 	if (kv_sink > 0) {
-		dispatch(encoder, "rotate_sink", kvar, (kv_sink * kv_dim / 64) * p->n_layers, 32, &(struct SinkArgs) { kv_dim, p->head_dim, p->rotary_dim, kv_sink, p->seq_len, log2(p->rope_theta) }, sizeof(struct SinkArgs), (void*[]) { s->key_cache }, 1);
+		dispatch(encoder, "rotate_sink", kvar, (kv_sink * kv_dim / 64) * p->n_layers, 32, 0, &(struct SinkArgs) { kv_dim, p->head_dim, p->rotary_dim, kv_sink, p->seq_len, log2(p->rope_theta) }, sizeof(struct SinkArgs), (void*[]) { s->key_cache }, 1);
 	}
 
 	// forward all the layers
@@ -218,41 +218,41 @@ float* forward_metal(struct Transformer* transformer, int token, int pos, unsign
 		size_t loff = (size_t)l * p->seq_len * kv_dim; // kv cache layer offset for convenience
 
 		// pre-attention rmsnorm
-		dispatch(encoder, "rmsnorm", NULL, 1, 1024, &(struct NormArgs) { dim, p->norm_eps, p->norm_ln }, sizeof(struct NormArgs), (void*[]) { s->xb, x, w->rms_att_weight[l] }, 3);
+		dispatch(encoder, "rmsnorm", NULL, 1, 1024, 0, &(struct NormArgs) { dim, p->norm_eps, p->norm_ln }, sizeof(struct NormArgs), (void*[]) { s->xb, x, w->rms_att_weight[l] }, 3);
 
 		// qkv
-		dispatch(encoder, "qkv", dkvar, (q_dim + kv_dim * 2) / 2, 32, &(struct QkvArgs) { dim, q_dim, kv_dim, p->head_dim, p->rotary_dim, pos, kv_pos, p->seq_len, loff, p->qkv_clip, log2(p->rope_theta) }, sizeof(struct QkvArgs), (void*[]) { s->xb, s->q, s->key_cache, s->value_cache, w->wq[l], w->wk[l], w->wv[l], w->bqkv[l] }, 8);
+		dispatch(encoder, "qkv", dkvar, (q_dim + kv_dim * 2) / 2, 32, 0, &(struct QkvArgs) { dim, q_dim, kv_dim, p->head_dim, p->rotary_dim, pos, kv_pos, p->seq_len, loff, p->qkv_clip, log2(p->rope_theta) }, sizeof(struct QkvArgs), (void*[]) { s->xb, s->q, s->key_cache, s->value_cache, w->wq[l], w->wk[l], w->wv[l], w->bqkv[l] }, 8);
 
 		// attn score
 		int kv_lent = (kv_len + 7) / 8;
 
-		dispatch(encoder, "attn_score", kvar, kv_lent * p->n_heads, 32, &(struct AttnArgs) { p->seq_len, kv_len, p->head_dim, kv_mul, p->n_heads, loff }, sizeof(struct AttnArgs), (void*[]) { s->att, s->q, s->key_cache }, 3);
+		dispatch(encoder, "attn_score", kvar, kv_lent * p->n_heads, 32, 0, &(struct AttnArgs) { p->seq_len, kv_len, p->head_dim, kv_mul, p->n_heads, loff }, sizeof(struct AttnArgs), (void*[]) { s->att, s->q, s->key_cache }, 3);
 
 		// attn softmax
-		dispatch(encoder, "attn_softmax", NULL, p->n_heads, 1024, &(struct AttnArgs) { p->seq_len, kv_len, p->head_dim, kv_mul, p->n_heads, loff }, sizeof(struct AttnArgs), (void*[]) { s->att }, 1);
+		dispatch(encoder, "attn_softmax", NULL, p->n_heads, 1024, 0, &(struct AttnArgs) { p->seq_len, kv_len, p->head_dim, kv_mul, p->n_heads, loff }, sizeof(struct AttnArgs), (void*[]) { s->att }, 1);
 
 		// attn mix
-		dispatch(encoder, "attn_mix", kvar, q_dim, 32, &(struct AttnArgs) { p->seq_len, kv_len, p->head_dim, kv_mul, p->n_heads, loff }, sizeof(struct AttnArgs), (void*[]) { s->q, s->att, s->value_cache }, 3);
+		dispatch(encoder, "attn_mix", kvar, q_dim, 32, 0, &(struct AttnArgs) { p->seq_len, kv_len, p->head_dim, kv_mul, p->n_heads, loff }, sizeof(struct AttnArgs), (void*[]) { s->q, s->att, s->value_cache }, 3);
 
 		// attn out
-		dispatch(encoder, "attn_out", dvar, dim, 32, (int[]) { q_dim }, sizeof(int), (void*[]) { x, s->q, w->wo[l] }, 3);
+		dispatch(encoder, "attn_out", dvar, dim, 32, 0, (int[]) { q_dim }, sizeof(int), (void*[]) { x, s->q, w->wo[l] }, 3);
 
 		if (!p->norm_par) {
 			// post-attention rmsnorm
-			dispatch(encoder, "rmsnorm", NULL, 1, 1024, &(struct NormArgs) { dim, p->norm_eps, p->norm_ln }, sizeof(struct NormArgs), (void*[]) { s->xb, x, w->rms_ffn_weight[l] }, 3);
+			dispatch(encoder, "rmsnorm", NULL, 1, 1024, 0, &(struct NormArgs) { dim, p->norm_eps, p->norm_ln }, sizeof(struct NormArgs), (void*[]) { s->xb, x, w->rms_ffn_weight[l] }, 3);
 		}
 
 		assert(p->n_experts == 0); // TODO
 
 		// ffn
-		dispatch(encoder, p->act_gelu ? "ffn13_gelu" : "ffn13_silu", dvar, hidden_dim, 32, (int[]) { dim }, sizeof(int), (void*[]) { s->hb, s->xb, w->w1[l], w->w3[l] }, 4);
-		dispatch(encoder, "ffn2", dvar, dim, 32, (int[]) { hidden_dim }, sizeof(int), (void*[]) { x, s->hb, w->w2[l] }, 3);
+		dispatch(encoder, p->act_gelu ? "ffn13_gelu" : "ffn13_silu", dvar, hidden_dim, 32, 0, (int[]) { dim }, sizeof(int), (void*[]) { s->hb, s->xb, w->w1[l], w->w3[l] }, 4);
+		dispatch(encoder, "ffn2", dvar, dim, 32, 0, (int[]) { hidden_dim }, sizeof(int), (void*[]) { x, s->hb, w->w2[l] }, 3);
 	}
 
 	// classifier into logits
 	if ((flags & FF_UPDATE_KV_ONLY) == 0) {
-		dispatch(encoder, "rmsnorm", NULL, 1, 1024, &(struct NormArgs) { dim, p->norm_eps, p->norm_ln }, sizeof(struct NormArgs), (void*[]) { s->xb, x, w->rms_final_weight }, 3);
-		dispatch(encoder, "output", dvar, p->vocab_size, 32, (int[]) { dim }, sizeof(int), (void*[]) { s->logits, s->xb, w->wcls }, 3);
+		dispatch(encoder, "rmsnorm", NULL, 1, 1024, 0, &(struct NormArgs) { dim, p->norm_eps, p->norm_ln }, sizeof(struct NormArgs), (void*[]) { s->xb, x, w->rms_final_weight }, 3);
+		dispatch(encoder, "output", dvar, p->vocab_size, 32, 0, (int[]) { dim }, sizeof(int), (void*[]) { s->logits, s->xb, w->wcls }, 3);
 	}
 
 	// submit commands and wait
