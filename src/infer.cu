74eec3705e2a12bf36f0ed7ef1fcc3a8aba7ac6f
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2024-06-07 10:54:46 -0700

    Need one more syncthreads before syncgrid for Mixtral
    Again not clear why any of this is necessary yet. One possibility is
    that the writes are buffered or cached, and we could write directly to
    L2 instead but that requires careful code modifications.

--- src/infer.cu
+++ src/infer.cu
@@ -483,6 +483,7 @@ __global__ __launch_bounds__(1024, 1) static void kernel_forward(const __grid_co
 			}
 		}
 
+		__syncthreads(); // TODO: unclear why this is needed for determinism
 		syncgrid();
 		coopstage(args.perfstats, 0);
 
